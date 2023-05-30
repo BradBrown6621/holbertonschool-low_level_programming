e75693acca7d9b719ff98144a55e90a557cb009c
root <6621@holbertonstudents.com>
2023-05-30 16:31:45 -0700

    Added files 6- and 7-

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * 
+ * @n: We will find the last digit of this num (and we will kill it /j)
+ *
+ * Return: <insert return value>
+ */
+
+int print_last_digit(int n);
+{
+	n = n % 10;
+	return (n);
+}
