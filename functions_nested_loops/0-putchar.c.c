39c75d12aa9dc4424d35a3bb26e8c01f269ef3d2
root <6621@holbertonstudents.com>
2023-05-30 08:20:29 -0700

    Added 0-

---
+++ functions_nested_loops/0-putchar.c.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 [Success]
+ */
+
+int main(void)
+{
+	char string[] = '_putchar'
+	
+	for int i in string
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
