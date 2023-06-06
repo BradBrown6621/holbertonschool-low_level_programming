3ce955c7cb283990a076204957c7327d76d881a8
root <6621@holbertonstudents.com>
2023-06-05 19:35:48 -0700

    hotfix

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,22 +10,27 @@ void print_diagonal(int n)
 {
 	int numlines = 0;
 	int numspaces = 0;
-
-	while (numlines <= n)
+	if (n == 0)
 	{
-		while (numspaces <= numlines)
-		{
-			if (numspaces >= numlines)
-			{
-				_putchar(92);
-			} else
-			{
-				_putchar(32);
-			}
-			numspaces++;
-		}
 		_putchar(10);
-		numlines++;
-		numspaces = 0;
+	} else
+	{
+		while (numlines < n)
+		{
+			while (numspaces <= numlines)
+			{
+				if (numspaces >= numlines)
+				{
+					_putchar(92);
+				} else
+				{
+					_putchar(32);
+				}
+				numspaces++;
+			}
+			_putchar(10);
+			numlines++;
+			numspaces = 0;
+		}
 	}
 }
