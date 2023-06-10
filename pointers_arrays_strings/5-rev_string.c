cb61f8944751dbf9be7b5c7783a26f9c79491992
root <6621@holbertonstudents.com>
2023-06-10 07:56:27 -0700

    hotfix

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include "2-strlen.c"
-#include "_putchar.c"
 
 /**
  * rev_string - Entry point
--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include "2-strlen.c"
-#include "_putchar.c"
 
 /**
  * puts_half - Entry point
