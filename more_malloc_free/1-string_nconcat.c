923e3f292d44998ccca50d67d3e6c509d5a80c1e
root <6621@holbertonstudents.com>
2023-07-04 14:33:32 -0700

    Added 1-

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - Concatenates n bytes of s2 to s1
+ *
+ * @s1: String to be concatenated to
+ * @s2: String to be concatenated from
+ * @n: Num of bytes to concat from s2 to s1
+ *
+ * Return: Result of concatenations (ptr) [SUCCESS]
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	int i;
+	unsigned int ii;
+	char *crawler;
+
+	if (strlen(s2) < n)
+	{
+		n = strlen(s2);
+	}
+
+	ptr = malloc ((strlen(s1) + n) + 1);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	crawler = *&s1;
+	i = 0;
+	ii = 0;
+
+	while (*crawler != 0)
+	{
+		ptr[i] = *crawler;
+		crawler++;
+		i++;
+	}
+
+	crawler = *&s2;
+	ii = 0;
+
+	while ((*crawler != 0) && (ii < n))
+	{
+		ptr[i] = *crawler;
+		crawler++;
+		ii++;
+		i++;
+	}
+
+	ptr[i] = 0;
+	
+	free(s1);
+	free(s2);
+
+	return(ptr);
+}
