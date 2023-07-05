3e1a1cc0d6b1b2ed8cecb40c5ff523435d347d2f
root <6621@holbertonstudents.com>
2023-07-05 15:47:21 -0700

    Fixed compilation errors of 1- and 2-

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
 #ifndef FuNC_PTRS
 #define FUNC_PTRS
 
-typedef unsigned int size_t;
-
 void print_name(char *name, void (*f)(char*));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
