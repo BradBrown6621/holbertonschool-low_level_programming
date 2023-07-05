0335b04c218c5dc276f5237af248e12069e36243
root <6621@holbertonstudents.com>
2023-07-04 18:51:50 -0700

    hotfix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOGS_H
 #define DOGS_H
 
+/**
+ * struct dog - Characteristics of dogs
+ *
+ * @name: The doggo's name
+ * @float: The doggo's age
+ * @owner: The doggo's owner
+ *
+ * Description: Characteristics of a dog
+ */
+
 struct dog
 {
 	char *name;
