b2f47b292dd63386e19b3bf56b5ef6728c1aedaa
root <6621@holbertonstudents.com>
2023-07-04 19:38:56 -0700

    Added 2-

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - Entry point
+ *
+ * @d: Description
+ *
+ * Return: NOTHING!!!
+ */
+
+void print_dog(struct dog *d)
+{
+	if ((*d).name != NULL)
+	{
+		printf("Name: %s\n", (*d).name);
+	} else
+	{
+		printf("Name: (nil)");
+	}
+
+	if ((*d).age != 0)
+	{
+		printf("Age: %f\n", (*d).age);
+	} else
+	{
+		printf("Age: (nil)");
+	}
+
+	if ((*d).owner != NULL)
+	{
+		printf("Owner: %s\n", (*d).owner);
+	} else
+	{
+		printf("Owner: (nil)");
+	}
+}
