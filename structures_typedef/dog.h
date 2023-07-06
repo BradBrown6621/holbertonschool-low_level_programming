d4047d5e0be84d6c9b4c1355191331a666a4acc8
root <6621@holbertonstudents.com>
2023-07-05 20:31:20 -0700

    hotfix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,20 +11,12 @@
  * Description: Characteristics of a dog
  */
 
-typedef struct dog dog_t;
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - Typedef for struct dog
- */
-
-
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
