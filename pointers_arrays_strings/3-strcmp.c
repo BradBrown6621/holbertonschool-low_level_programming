0bc1bdf5f5723134a73f185e29401cce0e043273
root <6621@holbertonstudents.com>
2023-07-05 23:45:28 -0700

    hotfix

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,8 +6,7 @@
  * @s1: Points to the string to be compared against
  * @s2: Points to the string to compare with
  *
- * Return: Positive num if s1 > s2, negative num if s1 < s2
- * Return: and 0 if otherwise
+ * Return: Positive num, negative nu 0 if otherwise
  */
 
 int _strcmp(char *s1, char *s2)
@@ -15,11 +14,14 @@ int _strcmp(char *s1, char *s2)
 	if (s1 > s2)
 	{
 		return (s1 - s2);
-	} else if (s1 < s2)
-	{
-		return (-1 * (s1 - s2));
 	} else
 	{
-		return (0);
+			if (s1 < s2)
+		{
+			return (-1 * (s1 - s2));
+		} else
+		{
+			return (0);
+		}
 	}
 }
---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string: Capitalizes all words
+ *
+ * @str: The uncapitalized string
+ *
+ * Return: Capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	char *ptr;
+	int i;
+
+	ptr = str;
+
+	for (i = 0; ptr[i] != 0; i++)
+	{
+		if ((ptr[i] >= 97) && (ptr[i] <= 122))
+		{
+			if ((ptr[i + 1] >= 97) && (ptr[i] <= 122))
+			{
+				ptr[i] = ptr[i] - 32;
+			}
+		}
+	}
+	return (*ptr);
+}
