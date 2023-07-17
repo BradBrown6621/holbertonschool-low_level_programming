b22195015db1e0179787eb32b194cf0d664165bf
root <6621@holbertonstudents.com>
2023-07-17 14:31:36 -0700

    hotfix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	const char *s = strdup(str);
+	char *s = strdup(str);
 	
 	list_t *newnode = malloc(sizeof(list_t));
 
@@ -23,7 +23,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	newnode->next = *head;
-	head = newnode;
+	*head = newnode;
 	newnode->str = s;
 	newnode->len = strlen(s);
 
