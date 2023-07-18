c0a00b6a467d8ff0cce5d05faab58194a9e02704
root <6621@holbertonstudents.com>
2023-07-17 21:08:57 -0700

    gave 0- the ability to process negative ints

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -11,6 +11,7 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *ptr = h;
+	int n;
 	int nodes;
 
 	if (h == NULL)
@@ -20,12 +21,13 @@ size_t print_listint(const listint_t *h)
 
 	for (nodes = 0; ptr != NULL; nodes++)
 	{
-		if (ptr->n < 0)
+		n = ptr->n;
+		if (n < 0)
 		{
-			ptr->n = ptr->n * -1;
+			n = n * -1;
 			putchar(45);
 		}
-		printf("%i\n", ptr->n);
+		printf("%i\n", n);
 		ptr = ptr->next;
 	}
 	return (nodes);
