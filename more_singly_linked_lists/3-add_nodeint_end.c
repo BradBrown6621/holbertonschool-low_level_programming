5b3b72ea21715ae266b0d4449af4269f01e06226
root <6621@holbertonstudents.com>
2023-07-17 21:51:27 -0700

    hotfix

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
  * Return: Pointer to the newly created node
  */
 
-listint_t *add_nodeint_end(list_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *ptr;
 
