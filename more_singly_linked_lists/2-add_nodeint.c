c31d16d413dec38c5a2de8e32139491a4357b134
root <6621@holbertonstudents.com>
2023-07-17 21:33:25 -0700

    hotfix

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnode = malloc(sizeof(listint_t));
 
-	if (newnode -- NULL)
+	if (newnode == NULL)
 	{
 		free(newnode);
 		return (NULL);
