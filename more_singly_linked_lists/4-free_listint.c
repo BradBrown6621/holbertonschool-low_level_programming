38547fa08c2c87bc6a3c830a87d88a5c8cce232f
root <6621@holbertonstudents.com>
2023-07-17 21:55:54 -0700

    Added 4-

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint - Calls free_helper
+ *
+ * @head: Start of list to free
+ *
+ * Return: NOTHING!!!
+ */
+
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	next = head->next;
+	free(head->n);
+	free(head);
+
+	free_list(next);
+}
