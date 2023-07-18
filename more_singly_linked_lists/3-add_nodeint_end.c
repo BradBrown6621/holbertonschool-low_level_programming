29cc30785b6eaa11c2f62d6baad215d6c2818fd5
root <6621@holbertonstudents.com>
2023-07-17 21:52:24 -0700

    hotfix

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *ptr;
 
-	listint_t *newnode = malloc(sizeof(list_t));
+	listint_t *newnode = malloc(sizeof(listint_t));
 
 	if (newnode == NULL)
 	{
