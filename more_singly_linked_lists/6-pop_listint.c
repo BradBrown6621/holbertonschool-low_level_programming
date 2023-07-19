78e1240569bb939410545a1f2be8170360c930bb
root <6621@holbertonstudents.com>
2023-07-18 20:53:20 -0700

    Added 6-

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pops the first node of the linked list
+ *
+ * @head: pointer to the start of the linked list
+ *
+ * Return: the head list's data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (*head == NULL)
+}
+		return (0);
+	}
+	n = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	return (n);
+}
