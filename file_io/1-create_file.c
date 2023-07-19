d18d8101c1cdb948a25e17c8e9b5d4703a3854e2
root <6621@holbertonstudents.com>
2023-07-18 17:54:58 -0700

    hotfix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file - Creates a new file and fills it with content (if it doesn't already exist)
+ * create_file - Creates a new file and fills it with content
  *
  * @filename: Name of file to be made
  * @text_content: Text to be added to new file
@@ -11,38 +11,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_CREAT | O_EXCL | O_RDWR, 0600);
-	char *buffer;
-	ssize_t textlen = strlen(text_content);
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	ssize_t bytesWritten;
 
 	if (fd == -1 || filename == NULL)
 	{
 		close(fd);
-		return (0);
+		return (-1);
 	}
 
-	buffer = malloc(textlen + 1);
-
-	if (buffer == NULL)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	strcpy(buffer, text_content);
-
-	bytesWritten = write(fd, buffer, textlen);
+	bytesWritten = write(fd, text_content, strlen(text_content));
 
 	if (bytesWritten == -1)
 	{
-		free(buffer);
 		close(fd);
-		return (0);
+		return (-1);
 	}
 
-	free(buffer);
 	close(fd);
-	return (bytesWritten);
+	return (1);
 }
