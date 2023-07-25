6d18882a55e59a5f325c9b8a7f0799bc937766f2
root <6621@holbertonstudents.com>
2023-07-24 21:58:51 -0700

    hotfix

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,19 +13,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *temp;
-	
+
 	if (!ht || !key)
 		return (NULL);
-	
+
 	index = key_index((const unsigned char *)key, ht->size);
-	
+
 	temp = ht->array[index];
-	
+
 	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
 			return (temp->value);
-				
+
 		temp = temp->next;
 	}
 
