6a2b1cfb33b412663b5b274c6a21319bb5537b74
root <root@BradWork>
2023-11-15 14:44:13 -0600

    Added 0-

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * print_dlistint - Prints dlistint_t to STDOUT
+ *
+ * @dlistint_t - Doubly linked list to print
+ *
+ * Return: Number of nodes as size_t
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%i\n", h->n);
+		if (h->next)
+		{
+			h = h->next;
+		}
+		else
+		{
+			return (nodes);
+		}
+	}
+	return (nodes);
+}
