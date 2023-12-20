1258405238582647d8991028c314580b18a6331f
BradBrown6621 <6621@holbertonstudents.com>
2023-12-20 13:49:25 -0600

    Added 4-

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char *value = NULL;
+	unsigned long int hashIndex;
+	hash_node_t *node = NULL;
+
+	hashIndex = (hash_djb2((const unsigned char *)key)) % ht->size;
+
+	node = ht->array[hashIndex];
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+
+	while (node->next != NULL)
+	{
+		node = node->next;
+	}
+	
+	value = node->value;
+	return (value);
+}
