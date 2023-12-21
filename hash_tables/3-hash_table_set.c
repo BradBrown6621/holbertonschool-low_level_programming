649aee327bfd97675bb05206c6d127bde65b68f9
BradBrown6621 <6621@holbertonstudents.com>
2023-12-20 19:33:32 -0600

    Attempted collision fix for 3-

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,12 +1,16 @@
 #include "hash_tables.h"
 
-void node_prepend(hash_node_t *node, hash_table_t *ht, unsigned long int index)
+hash_node_t *traverse_for_key(hash_node_t *node, const char *key)
 {
-	if (ht->array[index] != NULL)
+	if (node == NULL)
 	{
-		free(ht->array[index]);
+		return (NULL);
 	}
-	ht->array[index] = node;
+	if (strcmp(node->key, key) == 0)
+	{
+		return (node);
+	}
+	return(traverse_for_key(node->next, key));
 }
 
 int validate_params(hash_table_t *ht, const char *key, const char *value)
@@ -30,8 +34,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *key_cpy = NULL;
 	char *value_cpy = NULL;
 	hash_node_t *node = NULL;
+	hash_node_t *targetNode = NULL;
 
 	validate_params(ht, key, value);
+
+	index = key_index((const unsigned char *)key, size);
+
+	targetNode = traverse_for_key(ht->array[index], key);
+
+	if (ht->array[index] != NULL && targetNode != NULL)
+	{
+		targetNode->value = value_cpy;
+		return (1);
+	}
 
 	node = malloc(sizeof(hash_node_t));
 	key_cpy = malloc(strlen(key) + 1);
@@ -46,10 +61,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->key = key_cpy;
 	node->value = value_cpy;
 	node->next = NULL;
-
-	index = key_index((const unsigned char *)key, size);
-
-	node_prepend(node, ht, index);
 
 	return (1);
 }
